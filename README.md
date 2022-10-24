10a04c246a8db2e403e1b05984de0f34c000e89c
inshin <inshin@student.42seoul.kr>
2022-10-25 06:10:25 +0900

    ADD: gulp-pug,webserver,image,delete

---
+++ README.md
@@ -0,0 +1,7 @@
+# GULP
+
+## 초기 설정
+```
+npm install -g gulp-cli
+npm init
+```
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import { random } from './util';
+
+const randOne = random(10);
+const randTwo = random(20);
+
+console.log(`${randOne} ${randTwo}`);
