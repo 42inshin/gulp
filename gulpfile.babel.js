9678f23a557ca9daf9fee78c04356c79babd5859
inshin <inshin@student.42seoul.kr>
2022-11-09 08:57:17 +0900

    backup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,8 @@ import ws from 'gulp-webserver'; // web server
 import image from 'gulp-image';  // image optimize
 import autoprefixer from 'gulp-autoprefixer'; // css prefixer
 import miniCSS from 'gulp-csso'; // css optimize
+import bro from 'gulp-bro';
+import babelify from 'babelify'
 
 const sass = require("gulp-sass")(require("node-sass"));
 
@@ -22,6 +24,11 @@ const routes = {
     watch: "src/scss/**/*.scss",
     src: "src/scss/style.scss",
     dest: "build/css"
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js"
   }
 }
 
@@ -45,15 +52,23 @@ const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true
 
 const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+const js = () => gulp.src(routes.js.src).pipe(bro({
+  transform: [
+    babelify.configure({ presets: ["@babel/preset-env"] }),
+    ["uglifyify", { global: true }]
+  ]
+})).pipe(gulp.dest(routes.js.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
 }
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
